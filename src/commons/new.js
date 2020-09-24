7671dbde70dddf54408e306a643d7d9bf2338c05
Xiaohui Gan <286390860@qq.com>
2020-09-24 11:18:22 +0800

    update with upstream/master

--- src/commons/new.js
+++ src/commons/new.js
@@ -1,4 +1,7 @@
 // TODO: this file should replace commons/index.js
 export { default as UIComponent } from './UIComponent';
-export { default as asBaseComponent } from './asBaseComponent';
-export { default as forwardRef } from './forwardRef';
+export { default as asBaseComponent, BaseComponentInjectedProps } from './asBaseComponent';
+export { default as forwardRef, ForwardRefInjectedProps } from './forwardRef';
+export { default as withScrollEnabler, WithScrollEnablerProps } from './withScrollEnabler';
+export { default as withScrollReached, WithScrollReachedProps } from './withScrollReached';
+export { ContainerModifiers, MarginModifiers, TypographyModifiers, ColorsModifiers, BackgroundColorModifier } from './modifiers';
